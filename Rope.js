9c0e5bfde7e6f3c1e17bcfc172b98cccb1baacc1
SreenityaPonipireddy <75820726+SreenityaPonipireddy@users.noreply.github.com>
2021-02-11 21:08:42 -0600

    Add files via upload
    Review of Angry Birds Game

---
+++ Rope.js
@@ -0,0 +1,25 @@
+class Rope{
+constructor(bodyA, pointB){
+    var options={
+        bodyA: bodyA,
+        pointB: pointB,
+        stiffness:1.2,
+        length: 250
+    }
+    // VERY VERY VERY IMPORTANT
+    this.rope=Constraint.create(options);
+    this.pointB=pointB;
+    World.add(world, this.rope);
+
+}
+display(){
+var pointA = this.rope.bodyA.position;
+var pointB = this.pointB;
+push();
+strokeWeight(10);
+stroke("teal");
+line(pointA.x, pointA.y, pointB.x, pointB.y);
+
+pop();   
+}
+}
